18dd7b94e9f813138534d5800c53e7077dc23873
James Murty <james@murty.co>
2013-04-17 22:17:41 +0100

    Initial Pelican site configuration

---
+++ pelicanconf.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'James Murty'
+SITENAME = u'post past :: james murty'
+SITEURL = 'http://www.jamesmurty.com'
+FEED_DOMAIN = SITEURL
+
+TIMEZONE = 'Australia/Sydney'
+DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),
+          ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 6
